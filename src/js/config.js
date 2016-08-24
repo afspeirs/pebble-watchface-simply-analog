d6a327e2a3875cec5d54dca7db5c47f83c9e9336
afspeirs <afspeirs@users.noreply.github.com>
2016-08-24 22:41:22 +0100

    Added configurable background colour
    Implemented clay
    Added support for configurable background colour
      it will now change the colour of the hands, etc, to match the background colour. (eg. white on dark background and black on light background)

---
+++ src/js/config.js
@@ -0,0 +1,123 @@
+module.exports = [
+	{ 
+    	"type": "heading", 
+    	"defaultValue": "Simply Analog",
+		"size": 1,
+	},
+	{
+		"type": "section",
+		"items": [
+			{
+				"type": "heading",
+				"defaultValue": "Colour Selection"
+			},
+			{
+				"type": "color",
+				"messageKey": "COLOUR_BACKGROUND",
+				"label": "Background",
+				"defaultValue": "000000",
+				"sunlight": true
+			},
+// 			{
+// 				"type": "color",
+// 				"messageKey": "COLOUR_DATE",
+// 				"label": "Date",
+// 				"defaultValue": "FFFFFF",
+// 				"sunlight": true,
+// 				"capabilities": ["NOT_BW"],
+// 			},
+// 			{
+// 				"type": "color",
+// 				"messageKey": "COLOUR_HOUR",
+// 				"label": "Hour",
+// 				"defaultValue": "FFAA00",
+// 				"sunlight": true,
+// 				"capabilities": ["NOT_BW"],
+// 			},
+// 			{
+// 				"type": "color",
+// 				"messageKey": "COLOUR_MINUTE",
+// 				"label": "Minute",
+// 				"defaultValue": "FFFFFF",
+// 				"sunlight": true,
+// 				"capabilities": ["NOT_BW"],
+// 			},
+// 			{
+// 				"type": "color",
+// 				"messageKey": "COLOUR_BLUETOOTH",
+// 				"label": "Blutooth Disconnection",
+// 				"defaultValue": "FF0000",
+// 				"sunlight": true,
+// 				"capabilities": ["NOT_BW"],
+// 			}
+		]
+	},
+// 	{
+// 		"type": "section",
+// 		"items": [
+// 			{
+// 				"type": "heading",
+// 				"defaultValue": "Battery"
+// 			},
+// 			{
+// 				"type": "toggle",
+// 				"messageKey": "TOGGLE_BATTERY",
+// 				"label": "Show battery icon",
+// 				"defaultValue": false,
+// 			},
+// 			{
+// 				"type": "select",
+// 				"messageKey": "SELECT_BATTERY_PERCENT",
+// 				"defaultValue": "20",
+// 				"label": "Low battery percent",
+// 				"options": [
+// 					{ 
+// 						"label": "10%",
+// 						"value": "10" 
+// 					},
+// 					{ 
+// 						"label": "20%",
+// 						"value": "20" 
+// 					},
+// 					{ 
+// 						"label": "30%",
+// 						"value": "30" 
+// 					},
+// 					{ 
+// 						"label": "40%",
+// 						"value": "40" 
+// 					},
+// 					{ 
+// 						"label": "50%",
+// 						"value": "50" 
+// 					},
+// 					{ 
+// 						"label": "60%",
+// 						"value": "60" 
+// 					},
+// 				]
+// 			},
+// 		]
+//	},
+// 	{
+// 		"type": "section",
+// 		"items": [
+// 			{
+// 				"type": "heading",
+// 				"defaultValue": "Custom Text"
+// 			},
+// 			{
+// 				"type": "checkboxgroup",
+// 				"messageKey": "CHECK_DATE",
+// 				"label": "Select events to be displayed:",
+// 				"defaultValue": [false, false, false],
+// 				"options": ["New Years Day", "Halloween", "Christmas Day"]
+// 			}
+// 		]
+// 	},
+	{
+		"type": "submit",
+		"defaultValue": "Save"
+	}
+];
+
